da8b4ea8e3a3a22eba296afca983427f2d61807a
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-09-05 15:07:41 +0300

    added data to migration file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
 
   patch "/employees/:id" do
     employee = Employee.find(params[:id])
-    employee.update(salary: params[:salary])
+    employee.update(salary: params[:salary], username: params[:username])
     employee.to_json
   end
 
