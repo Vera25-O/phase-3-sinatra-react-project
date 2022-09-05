4b43e8cd98bc2a1f19f8f91f3c86daae33fedab5
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-09-05 17:20:20 +0300

    moditfied application_controller.rb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+  
   get "/employees" do
     employees = Employee.all.order(:created_at)
     employees.to_json
